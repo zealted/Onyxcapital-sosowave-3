213059d3633be345e88aeb7217f509d7146089bc
zealted <zealboniface2021@gmail.com>
2026-07-16 01:52:50 +0100

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: './',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          charts: ['recharts'],
+        }
+      }
+    }
+  }
+})
